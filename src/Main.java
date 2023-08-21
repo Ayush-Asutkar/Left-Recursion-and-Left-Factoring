c61026d70f720cac6d46b070a2ea8ce626b93ee1
Ayush-Asutkar <asutkarayush@gmail.com>
2023-08-21 19:00:35 +0530

    Added the functionality to main to handle third question

--- src/Main.java
+++ src/Main.java
@@ -32,42 +32,64 @@ public class Main {
         return grammar;
     }
 
-    private static void handleCode(Grammar grammar) {
+    private static void handleCode() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Following are the two functionality this code provides:");
         System.out.println("\t1.To remove left recursion\n" +
-                "\t2.To do left factoring\n");
+                "\t2.To do left factoring\n" +
+                "\t3.To check whether input string is accepted by a grammar");
 
-        System.out.print("Please enter you choice (either 1 or 2): ");
+        System.out.print("Please enter you choice (either 1 or 2 or 3): ");
         int choice = scanner.nextInt();
 
         System.out.println();
         switch (choice) {
-            case 1:
+            case 1: {
+                Grammar grammar = takeInputAndCreateGrammar();
+                System.out.println("Input rules:");
+                grammar.printRules();
+
                 grammar.applyAlgorithmForRemovalOfLeftRecursion();
                 System.out.println("After removal of left recursion:");
                 grammar.printRules();
                 break;
+            }
 
-            case 2:
+            case 2: {
+                Grammar grammar = takeInputAndCreateGrammar();
+                System.out.println("Input rules:");
+                grammar.printRules();
+
                 grammar.applyAlgorithmForProducingAnEquivalentLeftFactored();
                 System.out.println("After doing left factoring:");
                 grammar.printRules();
                 break;
+            }
 
-            default:
+            case 3: {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter the string: ");
+                String input = sc.nextLine();
+                RecursiveDescentParser recursiveDescentParser = new RecursiveDescentParser(input);
+                boolean check = recursiveDescentParser.applyParsing();
+                if (check) {
+                    System.out.println("The input string " + input + " is accepted");
+                } else {
+                    System.out.println("The input string " + input + " is not accepted");
+                }
+                break;
+            }
+
+            default: {
                 System.out.println("ERROR!!! Please enter a valid choice!");
                 System.out.println("Exiting...");
                 break;
+            }
         }
     }
 
     public static void main(String[] args) {
-        Grammar grammar = takeInputAndCreateGrammar();
-        System.out.println("Input rules:");
-        grammar.printRules();
-
-        handleCode(grammar);
+        handleCode();
     }
 }
